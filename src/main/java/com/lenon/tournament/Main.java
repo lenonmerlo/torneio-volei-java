111b9217d5b4ece920426a350cdba5d8706ed6fb
lenonmerlo <lenontm@gmail.com>
2025-09-09 09:30:52 -0300

    feat(app): add minimal Main with hello message

---
+++ src/main/java/com/lenon/tournament/Main.java
@@ -0,0 +1,7 @@
+package com.lenon.tournament;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Beach Volleyball Tournament (Java Collections) - booting up...");
+    }
+}
